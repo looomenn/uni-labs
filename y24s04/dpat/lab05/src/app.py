c14c9488f0f42f5b0bccc2c62bd5cac729dfc3c0
Bajraktar zi Lvovu <39407654+looomenn@users.noreply.github.com>
2024-06-06 22:36:35 +0300

    update: dpat/lab04 checkpoint3
    testing some stuff with settings

--- y24s04/dpat/lab05/src/app.py
+++ y24s04/dpat/lab05/src/app.py
@@ -9,34 +9,46 @@ import numpy as np
 import plotly.graph_objs as go
 import dash_bootstrap_components as dbc
 
-from dash import html, dcc, Input, Output, State
+from scipy.signal import iirfilter, lfilter, welch
+from dash import html, dcc, Input, Output, State, ctx
 
-dbc_css = "https://cdn.jsdelivr.net/gh/AnnMarieW/dash-bootstrap-templates/dbc.min.css"
+DBC_CSS = "https://cdn.jsdelivr.net/gh/AnnMarieW/dash-bootstrap-templates/dbc.min.css"
 
 app = dash.Dash(
     __name__,
-    meta_tags=[
-        {
-            "name": "viewport",
-            "content": "width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no",
-        }
-    ],
-    external_stylesheets=[dbc.themes.BOOTSTRAP, dbc_css]
+    title="Harmonic Visualisation",
+    external_stylesheets=[dbc.themes.BOOTSTRAP, DBC_CSS]
 )
 
-app.title = "Harmonic Visualisation"
-
-initial_values = {
-    'amplitude': {'value': 1.0, 'min': 0.1, 'max': 5.0},
-    'frequency': {'value': 1, 'min': 0.1, 'max': 10.0},
-    'phase': {'value': 0, 'min': 0.0, 'max': 2 * np.pi},
-    'noise_mean': {'value': 0.0, 'min': -1.0, 'max': 1.0},
-    'noise_covariance': {'value': 0.1, 'min': 0.0, 'max': 1.0},
-    'filter_order': {'value': 4, 'min': 0, 'max': 10},
-    'cutoff_frequency': {'value': 1.0, 'min': 0.01, 'max': 0.5}
+# Dict for default values of each input
+DEFAULTS: dict = {
+    'amplitude': {'value': 1.0, 'min': 0.1, 'max': 10.0, 'step': '?', 'category': 'function'},
+    'frequency': {'value': 1, 'min': 0.1, 'max': 10.0, 'step': '?', 'category': 'function'},
+    'phase': {'value': 0, 'min': 0.0, 'max': round(2 * np.pi), 'step': '?', 'category': 'function'},
+    'noise_mean': {'value': 0.0, 'min': -1.0, 'max': 1.0, 'step': '?', 'category': 'noise'},
+    'noise_covariance': {'value': 0.1, 'min': 0.0, 'max': 1.0, 'step': '?', 'category': 'noise'},
+    'filter_order': {'value': 4, 'min': 1, 'max': 10, 'step': '?', 'category': 'filter'},
+    'cutoff_frequency': {'value': 0.02, 'min': 0.01, 'max': 0.08, 'step': '?', 'category': 'fiter'}
 }
 
-ids = {'buttons': [], 'sliders': [], 'checklists': []}
+
+def calc_steps(
+        min_: float,
+        max_: float,
+        steps: float = 10
+) -> float:
+    """ Calculates step value (for dcc.Slider) based on the given min and max values """
+
+    raw = (max_ - min_) / steps
+    correlation = 10 ** np.floor(np.log10(raw))
+    res = raw / correlation
+
+    if res >= 5:
+        return 5 * correlation
+    elif res >= 2:
+        return 2 * correlation
+    else:
+        return correlation
 
 
 def build_header():
@@ -48,50 +60,47 @@ def build_header():
     )
 
 
-def build_sliders():
-    sliders = []
+def build_slider(
+        name: str,
+        config: dict
+):
+    step = config['step'] if '?' not in config['step'] \
+        else calc_steps(config['min'], config['max'], 5)
 
-    for key, value in initial_values.items():
-        slider_id = key + '-input'
-        ids['sliders'].append(slider_id)
-        step_size = round((value['max'] - value['min']) // 40)
+    slider_div_id = name + '-div'
 
-        slider = html.Div(
-            children=[
-                dbc.Label(key.replace('_', ' ').title()),
-                dcc.Slider(
-                    id=slider_id,
-                    min=value['min'],
-                    max=round(value['max']),
-                    step=step_size,
-                    value=value['value'],
-                    className="dbc mb-4",
-                    tooltip={"placement": "bottom", "always_visible": True},
-                )
-            ]
-        )
+    slider = html.Div(
+        id=slider_div_id,
+        className=f"mb-4",
+        children=[
+            dbc.Label(name.replace('_', ' ').title(), html_for=name),
+            dcc.Slider(
+                id=name,
+                className="dbc",
+                min=config['min'],
+                max=config['max'],
+                marks=None,
+                value=config['value'],
+                tooltip={"placement": "bottom", "always_visible": True},
+            )
+        ]
+    )
 
-        sliders.append(slider)
-
-    return sliders
+    return slider
 
 
 def build_switches():
-    ids['checklists'].append('switch-input')
-    # ids['checklists'].append('filter-input')
-    # ids['checklists'].append('several-input')
-
     return html.Div(
         children=[
             dbc.Label('Additional settings'),
             dbc.Checklist(
                 id='switch-input',
-                value=[1],
+                value=['show_noise'],
                 switch=True,
                 options=[
-                    {"label": "Show noise", "value": 1},
-                    {"label": "Show filter", "value": 2},
-                    {"label": "Split graphs", "value": 3},
+                    {"label": "Show noise", "value": 'show_noise'},
+                    {"label": "Show filter", "value": 'show_filter'},
+                    {"label": "Split graphs", "value": 'split_graph'},
                 ],
                 inline=True,
                 className="mb-4"
@@ -101,21 +110,18 @@ def build_switches():
 
 
 def build_buttons():
-    btn_random_id, btn_reset_id = 'random-button', 'reset-button'
-    ids['buttons'].append(btn_random_id), ids['buttons'].append(btn_reset_id)
-
     return html.Div(
         children=[
             dbc.Button(
                 "Randomise",
-                id=btn_random_id,
+                id='random-button',
                 color="primary",
                 className="ml-4",
                 n_clicks=0
             ),
             dbc.Button(
                 "Reset",
-                id=btn_reset_id,
+                id='reset-button',
                 color="danger",
                 n_clicks=0
             )
@@ -124,20 +130,241 @@ def build_buttons():
     )
 
 
+def build_sliders_card(name, config, category):
+
+    slider = build_slider(name, config)
+
+    return dbc.Card(
+        dbc.CardBody(
+            childer=[
+                html.H4(f'{category.title()} settings',
+                        className="card-title"),
+                html.Hr(),
+                dbc.Form(
+                    id=category + '-settings',
+                    children=[
+                        slider
+                    ]
+                )
+            ]
+        )
+    )
+
+
 def build_settings():
+    sliders: list = [build_slider(name, config) for name, config in DEFAULTS.items()]
+
     return dbc.Card(
         dbc.CardBody(
             children=[
                 html.H5('Function settings', className="card-title"),
                 html.Br(),
-                *build_sliders(),
-                html.Hr(),
-                build_switches(),
-                build_buttons()
+                dbc.Form(
+                    id='settings-form',
+                    children=[
+                        *sliders,
+                        html.Hr(),
+                        build_switches(),
+                        build_buttons()
+                    ]
+                ),
             ]
-        ),
-        id='settings'
+        )
     )
+
+
+@app.callback(
+    [
+        Output('noise_mean', 'disabled'),
+        Output('noise_covariance', 'disabled'),
+        Output('filter_order', 'disabled'),
+        Output('cutoff_frequency', 'disabled'),
+        Output('noise_mean-div', 'style'),
+        Output('noise_covariance-div', 'style'),
+        Output('filter_order-div', 'style'),
+        Output('cutoff_frequency-div', 'style'),
+    ],
+    [
+        Input('switch-input', 'value')
+    ]
+)
+def update_sliders(switches):
+    show_noise = 'show_noise' in switches
+    show_filter = 'show_filter' in switches
+
+    noise_div_style = {'opacity': '0.2'} if not show_noise else {'opacity': '1'}
+    filter_div_style = {'opacity': '0.2'} if not show_filter else {'opacity': '1'}
+
+    return [not show_noise, not show_noise, not show_filter,
+            not show_filter, noise_div_style, noise_div_style, filter_div_style, filter_div_style]
+
+
+@app.callback(
+    [
+        Output('amplitude', 'value'),
+        Output('frequency', 'value'),
+        Output('phase', 'value'),
+        Output('noise_mean', 'value'),
+        Output('noise_covariance', 'value'),
+        Output('filter_order', 'value'),
+        Output('cutoff_frequency', 'value')
+    ],
+    [
+        Input('random-button', 'n_clicks'),
+        Input('reset-button', 'n_clicks')
+    ],
+    [
+        State('amplitude', 'value'),
+        State('frequency', 'value'),
+        State('phase', 'value'),
+        State('noise_mean', 'value'),
+        State('noise_covariance', 'value'),
+        State('filter_order', 'value'),
+        State('cutoff_frequency', 'value')
+    ]
+)
+def update_inputs(random_btn, reset_btn, *args):
+    if 'reset-button' == ctx.triggered_id:
+        return [DEFAULTS[slider]['value'] for slider in DEFAULTS.keys()]
+
+    if 'random-button' == ctx.triggered_id:
+        return [
+            np.random.uniform(
+                DEFAULTS[slider]['min'],
+                DEFAULTS[slider]['max']
+            ) for slider in DEFAULTS.keys()
+        ]
+
+    return args
+
+
+noise_cache = {}
+
+
+@app.callback(
+    [
+        Output('graph', 'figure'),
+        Output('harmonic-graph', 'figure'),
+        Output('signal-graph', 'figure'),
+        Output('filter-graph', 'figure'),
+        Output('graph', 'style'),
+        Output('harmonic-graph', 'style'),
+        Output('signal-graph', 'style'),
+        Output('filter-graph', 'style')
+    ],
+    [
+        Input('amplitude', 'value'),
+        Input('frequency', 'value'),
+        Input('phase', 'value'),
+        Input('noise_mean', 'value'),
+        Input('noise_covariance', 'value'),
+        Input('filter_order', 'value'),
+        Input('cutoff_frequency', 'value'),
+        Input('switch-input', 'value')
+    ]
+)
+def update_graph(
+        amplitude,
+        frequency,
+        phase,
+        noise_mean,
+        noise_cov,
+        filter_order,
+        cutoff_freq,
+        switches
+):
+    show_noise = 'show_noise' in switches
+    show_filter = 'show_filter' in switches
+    split_graph = 'split_graph' in switches
+
+    noise_key = (noise_mean, noise_cov)
+    t = np.linspace(0, 2 * np.pi, 500)
+
+    if noise_key not in noise_cache:
+        noise_cache[noise_key] = np.random.normal(noise_mean, noise_cov, t.shape)
+
+    noise = noise_cache[noise_key]
+
+    harmonic = amplitude * np.sin(frequency * t + phase)
+    signal = harmonic + noise if show_noise else harmonic
+
+    nyquist_rate = 0.5
+    normalised_cutoff = cutoff_freq / nyquist_rate
+
+    filter_order = int(filter_order)
+
+    if show_filter:
+        b, a = iirfilter(
+            filter_order, normalised_cutoff, btype='low', ftype='butter', analog=False
+        )
+        filtered_signal = lfilter(b, a, signal)
+    else:
+        filtered_signal = signal
+
+    harmonic_graph = {
+        'data': [
+            go.Scatter(x=t, y=harmonic, mode='lines', name='Harmonic', line=dict(dash='dash'))
+        ],
+        'layout': {
+            'title': 'Harmonic Function',
+            'xaxis': {'title': 'Time'},
+            'yaxis': {'title': 'Amplitude'}
+        }
+    }
+
+    signal_fig = {
+        'data': [
+            go.Scatter(x=t, y=signal, mode='lines', name='Noisy Signal')
+        ],
+        'layout': {
+            'autosize': True,
+            'title': 'Noisy Signal',
+            'xaxis': {'title': 'Time'},
+            'yaxis': {'title': 'Amplitude'}
+        }
+    }
+
+    filtered_fig = {
+        'data': [
+            go.Scatter(x=t, y=filtered_signal, mode='lines', name='Filtered Signal')
+        ],
+        'layout': {
+            'autosize': True,
+            'title': 'Filtered Signal',
+            'xaxis': {'title': 'Time'},
+            'yaxis': {'title': 'Amplitude'}
+        }
+    }
+
+    combined_fig = {
+        'data': [
+            go.Scatter(x=t, y=harmonic, mode='lines', name='Harmonic', line=dict(dash='dash')),
+            go.Scatter(x=t, y=signal, mode='lines', name='Signal'),
+            go.Scatter(x=t, y=filtered_signal, mode='lines', name='Filtered Signal')
+        ],
+        'layout': {
+            'autosize': True,
+            'title': 'Harmonic Function with Noise',
+            'xaxis': {'title': 'Time'},
+            'yaxis': {'title': 'Amplitude'}
+        }
+    }
+
+    if split_graph:
+        combined_graph_style = {"display": "none"}
+        harmonic_graph_style = {"display": "block"}
+        signal_graph_style = {"display": "block"} if show_noise else {"display": "none"}
+        filtered_graph_style = {"display": "block"} if show_filter else {"display": "none"}
+    else:
+        combined_graph_style = {"display": "block"}
+        harmonic_graph_style = {"display": "none"}
+        signal_graph_style = {"display": "none"}
+        filtered_graph_style = {"display": "none"}
+
+    return [
+        combined_fig, harmonic_graph, signal_fig, filtered_fig,
+        combined_graph_style, harmonic_graph_style, signal_graph_style, filtered_graph_style
+    ]
 
 
 app.layout = dbc.Container(
@@ -147,15 +374,27 @@ app.layout = dbc.Container(
             children=[
                 dbc.Row(
                     children=[
-                        dbc.Col(build_settings(), md=4),
                         dbc.Col(
-                            dbc.Spinner(
-                                children=[
-                                    dcc.Graph(id='graph'),
-                                    html.Div(id='slider-output-container')
-                                ],
-                                delay_show=200
-                            )
+                            build_settings(),
+                            md=3
+                        ),
+                        dbc.Col(
+                            children=[
+                                dbc.Spinner(
+                                    children=[
+                                        dcc.Graph(id='graph'),
+                                        html.Div(
+                                            className="d-flex flex-column mh-30",
+                                            children=[
+                                                dcc.Graph(id='harmonic-graph', style={'display': 'none'}),
+                                                dcc.Graph(id='signal-graph', style={'display': 'none'}),
+                                                dcc.Graph(id='filter-graph', style={'display': 'none'})
+                                            ]
+                                        )
+                                    ],
+                                    delay_show=200
+                                )
+                            ]
                         )
                     ]
                 )
@@ -166,110 +405,6 @@ app.layout = dbc.Container(
     fluid=True,
     className="container-lg"
 )
-
-
-@app.callback(
-    [Output(slider_id, 'value') for slider_id in ids['sliders']],
-    [Input(button_id, 'n_clicks') for button_id in ids['buttons']],
-    [State(slider_id, 'value') for slider_id in ids['sliders']]
-)
-def update_inputs(*args):
-    ctx = dash.callback_context
-    if not ctx.triggered:
-        return args[-len(ids['sliders']):]
-    button_id = ctx.triggered[0]['prop_id'].split('.')[0]
-
-    if 'reset-button' in button_id:
-        return [
-            initial_values[
-                slider_id.split('-')[0]
-            ]['value'] for slider_id in ids['sliders']
-        ]
-
-    if 'random-button' in button_id:
-        return [
-            np.random.uniform(
-                initial_values[slider_id.split('-')[0]]['min'],
-                initial_values[slider_id.split('-')[0]]['max']
-            ) for slider_id in ids['sliders']
-        ]
-
-
-noise_cache = {}
-
-
-@app.callback(
-    Output('graph', 'figure'),
-    [Input(slider_id, 'value') for slider_id in ids['sliders']],
-    Input('switch-input', 'value')
-)
-def update_graph(*args):
-    slider_values = args[:-1]
-    amplitude, frequency, phase, noise_mean, noise_covariance, filter_order, cutoff_frequency = slider_values
-
-    switch_value = args[-1]
-
-    noise_key = (noise_mean, noise_covariance)
-    t = np.linspace(0, 2 * np.pi, 500)
-
-    if noise_key not in noise_cache:
-        noise_cache[noise_key] = np.random.normal(noise_mean, noise_covariance, t.shape)
-
-    noise = noise_cache[noise_key]
-
-    harmonic = amplitude * np.sin(frequency * t + phase)
-    signal = harmonic + noise if 1 in switch_value else harmonic
-
-    # Create the figure
-    figure = {
-        'data': [
-            go.Scatter(x=t, y=harmonic, mode='lines', name='Harmonic', line=dict(dash='dash')),
-            go.Scatter(x=t, y=signal, mode='lines', name='Signal')
-        ],
-        'layout': {
-            'title': 'Harmonic Function with Noise',
-            'xaxis': {'title': 'Time'},
-            'yaxis': {'title': 'Amplitude'}
-        }
-    }
-    return figure
-
-
-@app.callback(
-    Output('slider-output-container', 'children'),
-    [Input(slider_id, 'value') for slider_id in ids['sliders']],
-    [Input('switch-input', 'value')]
-)
-def update_output(*args):
-    values = args[:-1]
-    switch = args[-1]
-
-    tmp = []
-
-    for slider_id, value in zip(ids['sliders'], values):
-        label = slider_id.replace('_', ' ').replace('-input', '').title()
-        tmp.append(html.P(f'{label}: {value}'))
-
-    tmp.append(html.P(f'Show Noise: {switch}'))
-
-    return html.Div(
-        children=[
-            dbc.Button(
-                "Debug info",
-                id="hover-target",
-                color="secondary",
-                className="me-1",
-                n_clicks=0,
-            ),
-            dbc.Popover(
-                tmp,
-                target="hover-target",
-                body=True,
-                trigger="hover",
-            )
-        ]
-    )
-
 
 if __name__ == "__main__":
     app.run_server(debug=True)
